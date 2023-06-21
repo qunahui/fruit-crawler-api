23d83a132c0c3b839449a3d262742314bc3006bf
anmol-dash <115905646+anmol-dash@users.noreply.github.com>
2023-06-21 04:33:44 -0500

    Revert "Added autocomplete feature, developed backend"

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
 // const swaggerUi = require('swagger-ui-express');
 // const swaggerDocument = require('./src/swagger.json');
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
 
-require("dotenv").config({
+require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
 });
 
-require("./src/connections/mongodb-atlas");
+require('./src/connections/mongodb-atlas');
 
 const app = express();
 const port = process.env.PORT || 8000;
-const configRoute = require("./src/apis/index");
+const configRoute = require('./src/apis/index');
 
 app.use(express.json());
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
-app.get("/*", (req, res, next) => {
+app.get('/*', (req, res, next) => {
   res.set({
-    "Access-Control-Expose-Headers": "Content-Range",
-    "Content-Range": "1-2*",
-    "X-Total-Count": "30",
-    "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
+    'Access-Control-Expose-Headers': 'Content-Range',
+    'Content-Range': '1-2*',
+    'X-Total-Count': '30',
+    'Access-Control-Allow-Methods': 'DELETE, POST, GET, OPTIONS',
   });
   next();
 });
--- src/apis/index.js
+++ src/apis/index.js
@@ -1,9 +1,7 @@
-const userRoutes = require("./routers/user");
-const productRoutes = require("./routers/product");
-const paginationRoutes = require("./routers/pagination");
+const userRoutes = require('./routers/user');
+const productRoutes = require('./routers/product');
 
 module.exports = (app) => {
-  app.use("/users", userRoutes);
-  app.use("/products", productRoutes);
-  app.use("/pagination", paginationRoutes);
+  app.use('/users', userRoutes);
+  app.use('/products', productRoutes);
 };
